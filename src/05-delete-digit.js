7317d3bdaeedfbaa1732b174ed93339f9292d794
Vainila <workmail1421@gmail.com>
2021-04-10 18:53:54 +0300

    5

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,17 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
-}
+function deleteDigit(n) {
+  function find(val, ind, arr) {
+    if (ind === 0 && val < arr[ind + 1]) { return val; }
+    if (ind > 0 && (val < arr[ind + 1] && val < arr[ind - 1])) { return val; }
+    if (val < arr[ind - 1]) { return val; }
+    return false;
+  }
 
+  const newNum = (`${n}`).split('');
+  newNum[newNum.findIndex(find)] = '';
+  return +newNum.join('');
+}
+// console.log(deleteDigit(109));
 module.exports = deleteDigit;
